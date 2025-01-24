45f2da3226349d21c501f7ccfa7e2c0a7eea1302
Artem Kvasnetskyi <154246288+kvasnetskyi-adition@users.noreply.github.com>
2025-01-24 16:55:59 +0200

    Adding fake versions for the testing. Doc update script updated to update tutorial docs and _config.yml.

--- scripts/update_doc_versions.sh
+++ scripts/update_doc_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script checks the number of versions of the documentation and deletes the
-# oldest versions, after which it updates index.html with the version data.
+# oldest versions, after which it updates index.html and _config.yml with the version data.
 #
 # It receives one parameter - the maximum number of versions that should be stored.
 if [ -z "$1" ]; then
@@ -23,25 +23,35 @@ if [ ${#doc_version_folders[@]} -gt $versions_count ]; then
   for folder in "${folders_to_delete[@]}"; do
     echo "Deleting old version: $folder"
     rm -rf "docs/$folder"
+    rm -rf "docs/tutorials/_$folder"
   done
   doc_version_folders=("${doc_version_folders[@]:0:$versions_count}")
 fi
 
-# Generate string with versions to pass into index.html
-template_file="scripts/templates/index-template.html"
-output_file="docs/index.html"
-version_list=""
+# Generate strings with versions to pass into index.html and _config.yml
+html_template_file="scripts/templates/index-template.html"
+yml_template_file="scripts/templates/config-template.yml"
+html_output_file="docs/index.html"
+yml_output_file="docs/_config.yml"
+html_version_list=""
+jekyll_collection_list=""
+just_docs_collection_list=""
 
 for folder in "${doc_version_folders[@]}"; do
-  version_list+="<li><a href=\"$folder/\">SDK v.$folder</a></li>\n"
+  html_version_list+="<li><a href=\"$folder/\">SDK v.$folder</a></li>\n"
+  jekyll_collection_list+="\t$folder:\n\t\toutput: true\n"
+  just_docs_collection_list+="\t\t$folder:\n\t\t\tname: SDK v$folder\n\t\t\tnav_fold: true\n"
 done
-
-if [ ! -f "$template_file" ]; then
-  echo "❌ Error: Template file $template_file does not exist!"
-  exit 1
-fi
 
 # Update <!-- version_list --> inside index-template.html with the new versions
 # and pass updated content from index-template.html to index.html
-sed "s|<!-- version_list -->|$version_list|" "$template_file" > "$output_file"
+sed "s|<!-- version_list -->|$html_version_list|" "$html_template_file" > "$html_output_file"
 echo "✅ index.html updated with the new versions: $doc_version_folders"
+
+# Update <!-- jekyll_collection_list --> and <!-- just_docs_collection_list --> inside config-template.yml
+# with the new versions and pass updated content from config-template.yml to _config.yml
+yml_content=$(<"$yml_template_file")
+updated_yml_content=$(echo "$yml_content" | sed "s|<!-- jekyll_collection_list -->|$jekyll_collection_list|")
+updated_yml_content=$(echo "$updated_yml_content" | sed "s|<!-- just_docs_collection_list -->|$just_docs_collection_list|")
+echo "$updated_yml_content" > "$yml_output_file"
+echo "✅ _config.yml updated with the new versions: $doc_version_folders"
